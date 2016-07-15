2a503a6e79bb68fc1ff019d097da6fcf6901c0b1
xujiongbo <xujiongbo@gmail.com>
2016-07-15 21:46:08 +0800

    remove semi

--- timer.js
+++ timer.js
@@ -6,7 +6,7 @@
 
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame ||
         function(callback) {
-            setTimeout(callback, 1000 / 60);
+            setTimeout(callback, 1000 / 60)
         }
 
     function extend(dest, src){
