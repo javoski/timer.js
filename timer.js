4b1f3f5baf1fa19f998310583cb33e7ee2a56049
xujiongbo <xujiongbo@gmail.com>
2016-08-03 00:22:54 +0800

    add type check

--- timer.js
+++ timer.js
@@ -1,15 +1,15 @@
-;(function (global, factory){
+;(function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global.Timer = factory())
-}(this, function(){
+}(this, function() {
 
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame ||
         function(callback) {
             setTimeout(callback, 1000 / 60)
         }
 
-    function extend(dest, src){
+    function extend(dest, src) {
         for(var k in src){
             if(src.hasOwnProperty(k)){
                 dest[k] = src[k]
@@ -18,18 +18,21 @@
         return dest
     }
 
-    function Timer(options){
-        this.options = {
-            interval: 1000
+    function Timer(options) {
+        if(this instanceof Timer) {
+            this.options = {
+                interval: 1000
+            }
+            extend(this.options, options)
+        }else {
+            return new Timer(options)
         }
-        extend(this.options, options)
-        return this
     }
 
-    Timer.prototype.start = function(param){
-        if(typeof param === 'number'){
+    Timer.prototype.start = function (param) {
+        if(typeof param === 'number') {
             this.options.duration = param
-        }else if(typeof param === 'object'){
+        }else if(typeof param === 'object') {
             extend(this.options, param)
         }
         this.startTime = new Date().getTime()
@@ -39,21 +42,21 @@
         return this
     }
 
-    Timer.prototype.stop = function(){
+    Timer.prototype.stop = function () {
         this.stopFlag = true
         return this
     }
 
-    Timer.prototype._tick = function(){
+    Timer.prototype._tick = function () {
         var o = this.options
         var now = new Date().getTime()
         var current = Math.floor((now - this.startTime)/o.interval)
-        if(current >= o.duration || this.stopFlag){
+        if(current >= o.duration || this.stopFlag) {
             if(typeof o.finish === 'function')
                 o.finish(!this.stopFlag)
             return
         }
-        if(this.currentIndex !== current){
+        if(this.currentIndex !== current) {
             if(typeof o.ticking === 'function')
                 o.ticking(current, o.duration)
             this.currentIndex = current
